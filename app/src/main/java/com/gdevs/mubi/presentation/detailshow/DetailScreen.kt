495c35a55cca548a21378a96b3fdd0979a964c5d
Gustavo García Salas <gggustavogs@gmail.com>
2023-01-09 02:32:41 -0600

    Update detail screen design

--- app/src/main/java/com/gdevs/mubi/presentation/detailshow/DetailScreen.kt
+++ app/src/main/java/com/gdevs/mubi/presentation/detailshow/DetailScreen.kt
@@ -1,88 +1,96 @@
 package com.gdevs.mubi.presentation.detailshow
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import coil.request.ImageRequest
 import com.gdevs.mubi.R
 import com.gdevs.mubi.common.Resource
-import com.gdevs.mubi.data.remote.dto.CreatedBy
 import com.gdevs.mubi.data.remote.dto.Season
 import com.gdevs.mubi.data.remote.dto.TvShowDetailDto
+import com.gdevs.mubi.presentation.components.RatingBar
 import com.gdevs.mubi.presentation.navigation.AppScreens
-import com.gdevs.mubi.presentation.popularshow.PopularList
 import com.gdevs.mubi.presentation.ui.theme.Primary
 import com.gdevs.mubi.presentation.ui.theme.TextColor
 import com.gdevs.mubi.presentation.ui.theme.WhiteText
 import com.google.accompanist.coil.CoilImage
-import java.util.*
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun DetailScreen(
     showId: Int?,
     navController: NavController,
     viewModel: ShowDetailViewModel = hiltNavGraphViewModel()
 ) {
+
+    val lazyState = rememberLazyListState()
+    val visibleItem by remember {
+        derivedStateOf { lazyState.firstVisibleItemIndex >= 1 }
+    }
+
+    val showInfo = produceState<Resource<TvShowDetailDto>>(initialValue = Resource.Loading()) {
+        value = viewModel.getShowInfo(showId)
+    }.value
+
     Surface(
         color = MaterialTheme.colors.background,
         modifier = Modifier.fillMaxSize()
     ) {
 
         Scaffold(
-//            topBar = {
-//                TopAppBar(
-//                    navigationIcon = {
-//                        IconButton(
-//                            onClick = {
-//                                navController.popBackStack()
-//                            }
-//                        ) {
-//                            Icon(
-//                                imageVector = Icons.Filled.ArrowBack,
-//                                contentDescription = "back"
-//                            )
-//                        }
-//                    },
-//                    title = {
-//                        Text("Nombre de programa")
-//                    },
-//                    backgroundColor = Color.Transparent
-//                )
-//
-//            }
+            topBar = {
+                AnimatedVisibility(
+                    visible = visibleItem,
+                    enter = expandVertically(),
+                    exit = shrinkVertically()
+                ) {
+                    TopAppBar(
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+                                    navController.popBackStack()
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.ArrowBack,
+                                    contentDescription = "back"
+                                )
+                            }
+                        },
+                        title = {
+                            Text(showInfo.data!!.name)
+                        }
+                    )
+                }
+            }
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
             ) {
-                Details(showId = showId, navController = navController, viewModel = viewModel)
+                Details(showId = showId, navController = navController, showInfo = showInfo)
             }
         }
     }
@@ -93,48 +101,13 @@ fun DetailScreen(
 fun Details(
     showId: Int?,
     navController: NavController,
-    viewModel: ShowDetailViewModel
+    showInfo: Resource<TvShowDetailDto>
 ) {
 
-    val showInfo = produceState<Resource<TvShowDetailDto>>(initialValue = Resource.Loading()) {
-        value = viewModel.getShowInfo(showId)
-    }.value
-
-//    HeaderSection()
 
     ShowDetailStateWrapper(showId = showId, navController = navController, showInfo = showInfo)
 }
 
-@Composable
-fun HeaderSection() {
-    Box(
-        modifier = Modifier
-            .background(
-                color = Color.Red
-            )
-            .fillMaxWidth()
-            .height(232.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Default.ArrowBack,
-            contentDescription = null,
-            tint = Color.White,
-            modifier = Modifier
-                .size(36.dp)
-                .offset(16.dp, 16.dp)
-//                .clickable {
-//                    navController.popBackStack()
-//                }
-        )
-
-        Text(
-            text = "Show Title",
-            fontSize = 24.sp,
-            modifier = Modifier.layoutId("username"),
-            color = Color.White
-        )
-    }
-}
 
 @Composable
 fun ShowDetailStateWrapper(
@@ -182,51 +155,76 @@ fun ShowDetailSection(
             .fillMaxSize()
             .verticalScroll(scrollState)
     ) {
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
         ) {
-            CoilImage(
-                request = ImageRequest.Builder(LocalContext.current)
-                    .data("https://image.tmdb.org/t/p/w220_and_h330_face" + showInfo.posterPath)
-                    .build(),
-                contentDescription = showInfo.name,
-                fadeIn = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(232.dp),
-                alignment = Alignment.CenterStart,
-                contentScale = ContentScale.Crop
+            IconButton(
+                onClick = {
+                    navController.popBackStack()
+                }
             ) {
-                CircularProgressIndicator(
-                    color = MaterialTheme.colors.primary,
-                    modifier = Modifier.scale(0.6f)
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "back",
                 )
             }
             Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(8.dp),
+                    .fillMaxWidth(),
                 contentAlignment = Alignment.BottomStart
             ) {
-//                Column(
-//                    modifier = Modifier.background(color = Color.Red)
-//                ) {
+                CoilImage(
+                    request = ImageRequest.Builder(LocalContext.current)
+                        .data("https://image.tmdb.org/t/p/w220_and_h330_face" + showInfo.posterPath)
+                        .build(),
+                    contentDescription = showInfo.name,
+                    fadeIn = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(232.dp),
+                    alignment = Alignment.CenterStart,
+                    contentScale = ContentScale.Crop
+                ) {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier.scale(0.6f)
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = showInfo.originalName,
+                        fontWeight = FontWeight.Bold,
+                        color = WhiteText,
+                        style = MaterialTheme.typography.caption,
+                    )
                     Text(
                         text = showInfo.name,
                         fontWeight = FontWeight.Bold,
                         color = WhiteText,
                         style = MaterialTheme.typography.h4,
                     )
-//                }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        RatingBar(rating = showInfo.voteAverage.toFloat(), spaceBetween = 2.dp)
+                    }
+
+                }
             }
         }
 
         Column(
-//            horizontalAlignment = Alignment.Start,
             modifier = modifier
                 .fillMaxSize()
-//                .verticalScroll(scrollState)
                 .padding(8.dp)
         ) {
             Text(
