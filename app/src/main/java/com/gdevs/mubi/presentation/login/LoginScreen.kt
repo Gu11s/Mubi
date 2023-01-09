8965fb95f71d69fd69280a3649bf67467960a053
Gustavo García Salas <gggustavogs@gmail.com>
2023-01-09 01:23:47 -0600

    Adding firebase dependencies for login manage

--- app/src/main/java/com/gdevs/mubi/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/gdevs/mubi/presentation/login/LoginScreen.kt
@@ -3,7 +3,6 @@ package com.gdevs.mubi.presentation.login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,10 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
